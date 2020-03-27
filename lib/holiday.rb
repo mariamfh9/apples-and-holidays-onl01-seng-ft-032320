0bd22d8904ddadc7a8fb6ddf5af45c9f2f1f151e
Mariam Haider <mfh.haider@gmail.com>
2020-03-27 19:39:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,6 @@ add_new_season_and_holiday_with_supplies(holiday_supplies, :carnival, :party, ei
 
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter].values.flatten
 end
 
